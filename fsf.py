45047491623d1e63b630139e59c5d114138b0a9b
Johannes König <yoooxx@web.de>
2015-03-09 12:52:16 +0100

    create index and collect folders is working quite fine.
    everything else still needs to be done

---
+++ fsf.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+
+# FindSimilarFolders: find duplicate files and folders, that contain many of them
+# Copyright (C) 2015  Johannes König
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import argparse
+
+from fsf_core import create_index, collect_folders
+
+
+def prepare_create_index(args):	#todo: integrate collect_folders in create_index
+	print('create_index')
+
+	if args.exclude_path:
+		exclude = [item for sublist in args.exclude_path for item in sublist]	#flatten list of lists
+	else:
+		exclude = None
+
+	if args.exclude_pattern:
+		exclude_pattern = [item for sublist in args.exclude_pattern for item in sublist]
+	else:
+		exclude_pattern = None
+
+	if args.start_with:
+		start_at = args.start_with
+		start_after=False
+	elif args.start_after:
+		start_at = args.start_after
+		start_after=True
+	else:
+		start_at = ""
+		start_after=True
+
+	for rootdir in args.rootdir:
+		with open(args.index_file, 'a') as indexFile:
+			create_index(rootdir, indexFile, args.log_file, start_at, start_after, exclude, exclude_pattern, args.relative_to,  size_digits=13, verbosity=args.verbose)
+
+def prepare_collect_folders(args):
+	print('collect folders')
+
+	if args.exclude_path:
+		exclude = [item for sublist in args.exclude_path for item in sublist]	#flatten list of lists
+	else:
+		exclude = None
+
+	if args.exclude_pattern:
+		exclude_pattern = [item for sublist in args.exclude_pattern for item in sublist]
+	else:
+		exclude_pattern = None
+
+	if args.start_with:
+		start_at = args.start_with
+		start_after=False
+	elif args.start_after:
+		start_at = args.start_after
+		start_after=True
+	else:
+		start_at = ""
+		start_after=True
+
+	for rootdir in args.rootdir:
+		with open(args.collection_file, 'a') as indexFile:
+			collect_folders(rootdir = rootdir,
+								outfile = indexFile,
+								start_at = start_at,
+								start_after = start_after,
+								exclude = exclude,
+								exclude_pattern = exclude_pattern,
+								rel_to = args.relative_to,
+								fast = args.fast,
+								size_digits = 7,
+								verbosity = args.verbose,
+								serial = args.start_serial)
+
+def prepare_duplicate_files(args):
+	print('find duplicate files')
+
+def prepare_similar_folders(args):
+	print('find similar folders')
+
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='Find identical files and similar folders.')
+	subparsers = parser.add_subparsers(	title='subcommands',
+								description='valid subcommands:',
+								help='type %(prog)s SUBCOMMAND --help for additional help')
+
+
+
+	parser_create_index = subparsers.add_parser('createIndex',
+								aliases=['ci'],
+								description='walk through the given tree and store some information of each file in an index',
+								help='create the Index')
+
+	parser_create_index.add_argument('rootdir',
+								nargs='+',
+								help='Start indexing here. More than one dir may be given')
+	parser_create_index.add_argument('index_file',
+								help='file containing the index. new values will be appended')
+	parser_create_index.add_argument('-v', '--verbose',
+								nargs='?', const='2', default='1',
+								type=int, choices=range(0,4),
+								help='level of verbosity')
+	parser_create_index.add_argument('-e', '--exclude-path',
+								action='append',
+								nargs='+',
+								metavar='EXCL_PATH',
+								help='exclude %(metavar)ss from index')
+	parser_create_index.add_argument('-E', '--exclude-pattern',
+								action='append',
+								nargs='+',
+								metavar='EXCL_PATTERN',
+								help='exclude %(metavar)ss from index')
+	start_group = parser_create_index.add_mutually_exclusive_group()
+	start_group.add_argument('-s', '--start-after',
+								help='start indexing after given file or folder. usefull for continuing interupted run')
+	start_group.add_argument('-S', '--start-with',
+								help="start indexing with given file or folder. Only either '--start-with' or '--start-after' may be given")
+	parser_create_index.add_argument('-l', '--log-file')
+	parser_create_index.add_argument('-R', '--relative-to',
+								metavar='REL_PATH',
+								help='all paths in the index file are relative to %(metavar)s')
+
+	parser_create_index.set_defaults(func=prepare_create_index)
+
+
+
+	parser_collect_folders = subparsers.add_parser('collectFolders',
+								aliases=['cf'],
+								help='create a list of all folders. This should be done when creating the index, but can be done standalon as well with this command')
+
+	parser_collect_folders.add_argument('rootdir',
+								nargs='+',
+								help='Start collecting here. More than one dir may be given')
+	parser_collect_folders.add_argument('collection_file',
+								help='file containing the folder collection. new values will be appended')
+	parser_collect_folders.add_argument('-v', '--verbose',
+								nargs='?', const='2', default='1',
+								type=int, choices=range(0,4),
+								help='level of verbosity')
+	fast_group = parser_collect_folders.add_mutually_exclusive_group()
+	fast_group.add_argument('-f', '--fast',
+						#		nargs=0,
+								default=False,
+								action='store_true',
+								help="if set, don't check, if files are readable. must not occur together with -E/--exclude_pattern")
+	parser_collect_folders.add_argument('-e', '--exclude-path',
+								action='append',
+								nargs='+',
+								metavar='EXCL_PATH',
+								help='exclude %(metavar)ss from index')
+	fast_group.add_argument('-E', '--exclude-pattern',
+								action='append',
+								nargs='+',
+								metavar='EXCL_PATTERN',
+								help='exclude %(metavar)ss from index. must not occur together with -f/--fast')
+	start_group2 = parser_collect_folders.add_mutually_exclusive_group()
+	start_group2.add_argument('-s', '--start-after',
+								help='start indexing after given file or folder. usefull for continuing interupted run')
+	start_group2.add_argument('-S', '--start-with',
+								help="start indexing with given file or folder. Only either '--start-with' or '--start-after' may be given")
+	parser_collect_folders.add_argument('-R', '--relative-to',
+								metavar='REL_PATH',
+								help='all paths in the colltion file are relative to %(metavar)s')
+	parser_collect_folders.add_argument('--start-serial',
+								default = 1,
+								type=int,
+								metavar='START_SERIAL',
+								help='start with serial at %(metavar)s. Usefull with --start-with/--start-after')
+	parser_collect_folders.set_defaults(func=prepare_collect_folders)
+
+
+
+	parser_duplicate_files = subparsers.add_parser('duplicateFiles',
+								aliases=['df'],
+								help='find duplicate files in the index')
+
+	parser_duplicate_files.add_argument('index_file',
+								nargs='+',						#todo: make it work with more than one file
+								help='file(s) to look for duplikates. More than one file may be given')
+	parser_duplicate_files.add_argument('duplicatelist',
+								help='file to write the duplikates to')
+	parser_duplicate_files.add_argument('-v', '--verbose',
+								nargs='?', const='2', default='1',
+								type=int, choices=range(0,4),	#todo: what range suits?
+								help='level of verbosity')
+
+	parser_duplicate_files.set_defaults(func=prepare_duplicate_files)
+
+
+
+	parser_similar_folders = subparsers.add_parser('similarFolders',
+								aliases=['sf'],
+								help='find similar folders (folders that contain many duplicate files)')
+
+	parser_similar_folders.set_defaults(func=prepare_similar_folders)
+
+
+
+	args=parser.parse_args()
+	args.func(args)
---
+++ fsf_core.py
@@ -0,0 +1,264 @@
+# FindSimilarFolders: find duplicate files and folders, that contain many of them
+# Copyright (C) 2015  Johannes König
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+import hashlib
+import pathlib
+
+def _gethash(filename, blocksize=65536):
+	hasher=hashlib.sha1()
+	with open(filename, "rb") as f:
+		for block in iter(lambda: f.read(blocksize), b''):
+			hasher.update(block)
+	return hasher.hexdigest()
+
+
+def create_index(rootdir, outfile, start_at="", start_after=True, exclude=[], exclude_pattern=[], rel_to=None, size_digits=13, verbosity=2):
+	""" walk down the tree from rootdir (exclude 'exclude'. start at 'start_at' to continue
+	a previous run (if Start_after==True, start with the next file, otherwise start with the given file))
+	for each file calculate its checksum. append file statistics to 'outfile'
+	as follows, separated by "\t":
+	filesize (use 'sizedigits' digits)	mtime	checksum	path (relative to 'relto')
+	if verbosity =  0: print nothing
+					1: print each folder
+					2: print each file
+					3: print each line """
+
+	#todo: make 'start_at' find its start faster
+
+
+	start_at_file = start_at and os.path.isfile(start_at)
+	start_at_dir  = start_at and os.path.isdir(start_at)
+	show_message  = True
+
+
+	# walk through the whole tree
+	for root, dirs, files in os.walk(rootdir):
+		if verbosity == 1:
+			print(root)
+		if verbosity > 1:
+			print('\033[93m' + root + '\033[0m')
+
+
+		# skip until reach start_at (or dir after)
+		if start_at_dir:
+			if verbosity >= 1 and show_message:
+				print("\033[94mskip until dir " + start_at + "\033[0m")
+				show_message = False
+			if os.path.samefile(root, start_at):
+				start_at_dir = False
+				print("\033[94mskipping to dir finished\033[0m")
+				show_message = True
+				if start_after:
+					dirs.clear()
+					continue
+			else:
+				continue
+
+
+		# skip dirs, that match with exclude (and subdirs)
+		if exclude:
+			for path in exclude:
+				if os.path.samefile(root, path):
+					if verbosity >= 1:
+						print("\033[94mexlude dir: " + path + "\033[0m")
+					dirs.clear()
+					files.clear()
+					continue
+
+
+
+		for name in files:
+			fullname = os.path.join(root, name)
+
+			if start_at_file:	# skip until reach start_at (or file after)
+				if verbosity >= 1 and show_message:
+					print("\033[94mskip until file " + start_at + "\033[0m")
+					show_message = False
+				if os.path.exists(fullname) and os.path.samefile(fullname, start_at):
+					start_at_file = False
+					print("\033[94mskipping to file finished\033[0m")
+					show_message = True
+					if start_after:
+						continue
+				else:
+					continue
+
+			if os.path.islink(fullname):		# don't resove links
+				if verbosity >= 2:
+					print("\033[94mis link: " + fullname + "\033[0m")
+				continue
+			if not os.path.isfile(fullname):	# is it a file at all?
+				if verbosity >= 2:
+					print("\033[94mnot a file: " + fullname + "\033[0m")
+				continue
+
+
+
+			if exclude_pattern:			# skip files, that match with exclude_pattern
+				skip=False
+				for pattern in exclude_pattern:
+					if pathlib.Path(fullname).match(pattern):
+						if verbosity >= 2:
+							print("\033[94mexclude file " + name + ", matches pattern ", pattern, "\033[0m")
+						skip=True
+				if skip:
+					continue
+
+
+			fstats = os.stat(fullname)
+
+			try:
+				fhash = _gethash(fullname)
+
+			except PermissionError as e:
+				print ("\033[91mPermission Error: " + fullname + "\033[0m")
+				if errorfile:
+					with open(errorfile, "a") as errf:
+						errf.write("Permission Error: "+ fullname + "\n")
+				continue
+
+			except FileNotFoundError as e:
+				print ("\033[91mFile not found Error: " + fullname + "\033[0m")
+				if errorfile:
+					with open(errorfile, "a") as errf:
+						errf.write("file not found Error: "+ fullname + "\n")
+				continue
+
+			except Exception as e:
+				print ("\033[91munhandled Exception: " + fullname + "\033[0m")
+				errstr = str(e)
+				if errorfile:
+					with open(errorfile, "a") as errf:
+						errf.write(fullname + "  " + errstr + "\n")
+				continue
+
+
+			line = "{size: {digits}d}\t{mtime: 10.4f}\t{checksum}\t{path}".format(
+				digits = size_digits,
+				size = fstats.st_size,
+				mtime = fstats.st_mtime,
+				checksum = fhash,
+				path = ( os.path.relpath(fullname, rel_to) if rel_to!=None else fullname))
+
+			outfile.write(line+'\n')
+
+			if verbosity == 2:
+				print(name)
+			if verbosity >= 3:
+				print(line)
+
+
+
+def collect_folders(rootdir, outfile, start_at="", start_after=True, exclude=[], exclude_pattern=[], rel_to=None, fast=False, size_digits=6, verbosity=2, serial = 1):
+	""" walk down the tree from rootdir (exclude 'exclude'. start at 'start_at' to continue
+	a previous run (if Start_after==True, start with the next file, otherwise start with the given file))
+	for each folder count its files. append file statistics to 'outfile'
+	as follows, separated by "\t":
+	serial number		path (relative to 'relto')	number of items		number of readable files
+	if verbosity =  0: print nothing
+					1: print each folder
+					2: print each folder messages
+					3: print each line """
+
+	#todo: make 'start_at' find its start faster
+
+
+	start_at_file = start_at and os.path.isfile(start_at)
+	start_at_dir  = start_at and os.path.isdir(start_at)
+	show_message  = True
+	serial -= 1
+
+	if start_at_file:
+		raise ValueError('start_at has to be a dir, not a file!')
+
+
+	# walk through the whole tree
+	for root, dirs, files in os.walk(rootdir):
+		if verbosity == 1:
+			print(root)
+		if verbosity > 1:
+			print('\033[93m' + root + '\033[0m')
+
+
+		# skip until reach start_at (or dir after)
+		if start_at_dir:
+			if verbosity >= 1 and show_message:
+				print("\033[94mskip until dir " + start_at + "\033[0m")
+				show_message = False
+			if os.path.samefile(root, start_at):
+				start_at_dir = False
+				print("\033[94mskipping to dir finished\033[0m")
+				show_message = True
+				if start_after:
+					dirs.clear()
+					continue
+			else:
+				continue
+
+
+		# skip dirs, that match with exclude (and subdirs)
+		if exclude:
+			skip=False
+			for path in exclude:
+				if os.path.samefile(root, path):
+					if verbosity >= 1:
+						print("\033[94mexlude dir: " + path + "\033[0m")
+					dirs.clear()
+					files.clear()
+					skip=True
+			if skip:
+				continue
+
+		number_real_files=0
+		if not fast:
+			for name in files:
+				fullname = os.path.join(root, name)
+
+				if os.path.islink(fullname):		# don't resove links
+					if verbosity >= 2:
+						print("\033[94mis link: " + fullname + "\033[0m")
+					continue
+				if not os.path.isfile(fullname):	# is it a file at all?
+					if verbosity >= 2:
+						print("\033[94mnot a file: " + fullname + "\033[0m")
+					continue
+
+				if exclude_pattern:			# skip files, that match with exclude_pattern
+					skip=False
+					for pattern in exclude_pattern:
+						if pathlib.Path(fullname).match(pattern):
+							if verbosity >= 2:
+								print("\033[94mexclude file " + name + ", matches pattern ", pattern, "\033[0m")
+							skip=True
+					if skip:
+						continue
+
+				number_real_files += 1
+
+		serial += 1
+		line = "{serial: {serdigits}d}\t{path}\t{numfiles: {digits}d}\t{numrealfiles: {digits}d}".format(
+					serdigits = size_digits + 3,
+					digits = size_digits,
+					serial =serial,
+					numfiles = len(files),
+					numrealfiles = -1 if fast else number_real_files,
+					path = ( os.path.relpath(root, rel_to) if rel_to!=None else root))
+
+		outfile.write(line+'\n')
+		if verbosity >= 3:
+			print(line)
